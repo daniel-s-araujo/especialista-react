60695bec7bc391e04fb0a93ddc5448aab4d6f498
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-02-26 20:45:20 -0300

    2.9 JSX também é uma expressão

--- erjs/src/HelloWorld.tsx
+++ erjs/src/HelloWorld.tsx
@@ -1,11 +1,14 @@
 export default function HelloWorld () {
   const name = 'Daniel'
+  const title = <h1>Hello, {name}!</h1>
+
+  if (name === 'Daniel') {
+    return <div>
+      <h1 style={{ background: 'red' }}>{ name }</h1>
+    </div>
+  }
 
   return <div>
-    <h1>Hello,
-      { name === 'Daniel'
-          ? 'Danilo'
-          : name
-      }!!!</h1>
+    {title}
   </div>
 }
