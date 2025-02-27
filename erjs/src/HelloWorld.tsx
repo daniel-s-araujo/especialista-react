e7f5ccdcbca5feba05fef14f7a86b0aac0b78542
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-02-27 20:49:50 -0300

    2.10 Atributos com JSX

--- erjs/src/HelloWorld.tsx
+++ erjs/src/HelloWorld.tsx
@@ -1,10 +1,10 @@
 export default function HelloWorld () {
   const name = 'Daniel'
   const title = <h1>Hello, {name}!</h1>
-
+  const index = 0
   if (name === 'Daniel') {
     return <div>
-      <h1 style={{ background: 'red' }}>{ name }</h1>
+      <h1 tabIndex={index} style={{ background: 'red' }}>{ name }</h1>
     </div>
   }
 
