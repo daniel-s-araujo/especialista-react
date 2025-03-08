731ccdc08e2ab5025359b1327c7c06ded65503e9
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-03-08 09:50:10 -0300

    2.28 Manipulação de eventos

--- erjs/src/App.tsx
+++ erjs/src/App.tsx
@@ -1,27 +1,25 @@
-import { useState } from 'react';
 import './App.css';
-import Card from './components/Card';
-import Post, { PostObject } from './components/Post';
 
 function App() {
   console.log('renderizou app')
-  const [post, setPost] = useState<PostObject | undefined>({
-    title: 'Título maneiro',
-    content: 'Lorem Ipsum dolor sit amet'
-  })
+  
+  function handleClick (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault()
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        {
-          !!post &&
-            <Card align="left" title={"Card"}>
-              <Post post={post} totalComments={12} />
-            </Card>
-        }
-        <button onClick={() => setPost(undefined)}>
-          Remover post
-        </button>
+
+        <form action="/test">
+
+          <button
+            type="submit"
+            onClick={handleClick}
+          >
+            Clique em mim
+          </button>
+        </form>
       </header>
     </div>
   );
