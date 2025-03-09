763843e3f1448573934d587740f90996b6b5bdeb
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-03-09 08:13:16 -0300

    3.2 "Ciclos de vida" com o hook useEffect

--- erjs/src/index.tsx
+++ erjs/src/index.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { useState } from 'react';
+
+function Batata () {
+  const [canViewApp, setCanViewApp] =
+    useState(true)
+  return <>
+    {
+      canViewApp &&
+        <App />
+    }
+    <button
+      onClick={() => {
+        setCanViewApp(!canViewApp)
+      }}
+    >
+      toggle
+    </button>
+  </>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Batata />
   </React.StrictMode>,
   document.getElementById('root')
 );
