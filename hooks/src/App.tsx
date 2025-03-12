f282cabd2bfcee773cb2577fac4166363e0761ae
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-03-12 20:37:21 -0300

    3.8 Gerenciamento de estado complexo com useReducer

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,19 +1,53 @@
-import { useEffect, useRef } from "react";
-import "./App.css";
-import Button from "./Button";
+import { useReducer } from 'react';
+import './App.css';
+
+interface InitialState { count: number }
+
+type Action =
+  { type: 'DECREMENT' } |
+  { type: 'INCREMENT', payload: number }
+
+const initialState: InitialState = {
+  count: 1
+}
+
+function reducer (state: InitialState, action: Action): InitialState {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.payload
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count -1 
+      }
+    default:
+      return state
+  }
+}
 
 function App() {
-  const button = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      button.current?.click();
-    }, 2000);
-  }, []);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <div className="App">
-      <Button ref={button} title="clique em mim" />
+      <div style={{ backgroundColor: 'peachpuff' }}>
+        { state.count }
+      </div>
+      <button
+        onClick={() => {
+          dispatch({ type: 'INCREMENT', payload: 2 })
+        }}
+      >
+        acrescer
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: 'DECREMENT' })
+        }}
+      >
+        diminuir
+      </button>
     </div>
   );
 }
